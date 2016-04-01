755851b4bf35487d95415b4f93f2e24927ae21b8
Vladlen <vcmariorocks@gmail.com>
2016-04-01 15:18:12 -0600

    Almost done with board class
    Added the boardTest that margret provided and that got most of the
    issues with board I had. There is still issues with the iterable and the
    test is solvable false, but hopefully i can atleast get if the board is
    solvable by today

--- src/a04/Board.java
+++ src/a04/Board.java
@@ -6,19 +6,23 @@ import edu.princeton.cs.algs4.Queue;
 
 public class Board
 {
-	private int[][] board;
-	private int size;
+	private final int[][] board;
+	private final int size;
 
 	// construct a board from an N-by-N array of blocks
 	// (where blocks[i][j] = block in row i, column j)
 	public Board(int[][] blocks)
 	{
 		// we check if we have a square puzzle because im sure having anything
-		// other than square will be difficult to solve lmao.
-		if (blocks[0].length == blocks.length)
+		// other than square will be difficult to solve lmao
+		size = blocks.length;
+		this.board = new int[size][size];
+		for (int i = 0; i < size; i++)
 		{
-			board = blocks;
-			size = blocks.length;
+			for (int j = 0; j < size; j++)
+			{
+				board[i][j] = blocks[i][j];
+			}
 		}
 
 	}
@@ -33,8 +37,8 @@ public class Board
 	// number of blocks out of place
 	public int hamming()
 	{
-		int[] $OneDBoard = OneDimentionalConversion();
-		int counter = 1; // This checks for 
+		int[] $OneDBoard = OneDimentionalConversion(board);
+		int counter = 1; // This checks for
 		int outofplace = 0;
 
 		for (int i = 0; i < $OneDBoard.length; i++)
@@ -44,7 +48,10 @@ public class Board
 				counter++;
 			} else if ($OneDBoard[$OneDBoard.length - 1] == 0 && counter == $OneDBoard.length)
 			{
-				
+
+			} else if ($OneDBoard[i] == 0)
+			{
+				counter++;
 			} else
 			{
 				counter++;
@@ -57,14 +64,36 @@ public class Board
 	// sum of Manhattan distances between blocks and goal
 	public int manhattan()
 	{
-		return 0;
+		int manhattan = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (board[i][j] == 0)
+					continue;
+				int positiony = board[i][j] % size;
+				int positionx = board[i][j] / size;
+				if (positiony == 0)
+				{
+					positiony = size - 1;
+					--positionx;
+				} else
+				{
+					--positiony;
+				}
+				manhattan += Math.abs(positionx - i) + Math.abs(positiony - j);
+			}
+		}
+
+		return manhattan;
 
 	}
 
 	// is this board the goal board?
 	public boolean isGoal()
 	{
-		if(hamming() == 0)
+		if (hamming() == 0)
 			return true;
 		else
 			return false;
@@ -73,14 +102,68 @@ public class Board
 	// is this board solvable?
 	public boolean isSolvable()
 	{
-		return false;
+		if(size % 2 == 0)
+		{
+			Point zeropoint = FindIndexOfElement(0);
+			if(manhattan() + zeropoint.getY() % 2 != 0)
+				return true;
+			else
+				return false;
+		}
+		else
+		{
+			if(manhattan() % 2 == 0)
+				return true;
+			else
+				return false;
+			
+		}
 
 	}
 
 	// does this board equal y?
 	public boolean equals(Object y)
 	{
+		Board otherBoard = (Board) y;
+		if (y == this)
+		{
+			return true;
+		}
+		if (null == y)
+		{
+			return false;
+		}
+		if (!this.getClass().equals(y.getClass()))
+		{
+			return false;
+		}
+		if (this.size() != otherBoard.size())
+		{
+			return false;
+		}
+		if (this.hamming() != otherBoard.hamming())
+		{
+			return false;
+		}
+		int[] thisClass = OneDimentionalConversion(board);
+		int[] thatClass = OneDimentionalConversion(otherBoard.board);
+		if (thisClass.length == thatClass.length)
+		{
+			boolean stillTrue = true;
+			for (int i = 0; i < thisClass.length; i++)
+			{
+				if (i == thisClass.length - 1 && thisClass[i] == thatClass[i])
+				{
+					return stillTrue;
+				} else if (thisClass[i] == thatClass[i])
+				{
+					stillTrue = true;
+				} else
+					return false;
+			}
+		}
 		return false;
+
 	}
 
 	// all neighboring boards
@@ -94,18 +177,17 @@ public class Board
 	// below)
 	public String toString()
 	{
-		String returnable = size + " \n";
-
+		StringBuilder s = new StringBuilder();
+		s.append(size + "\n");
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++)
 			{
-				returnable += board[i][j] + " ";
+				s.append(String.format("%2d ", board[i][j]));
 			}
-			returnable += "\n";
+			s.append("\n");
 		}
-
-		return returnable;
+		return s.toString();
 	}
 
 	private class NeighborIterator implements Iterable<Board>
@@ -115,12 +197,36 @@ public class Board
 		public Iterator<Board> iterator()
 		{
 			Queue<Board> queue = new Queue<Board>();
-			
-			
-			
+
+			Point zeroIndex = FindIndexOfElement(0);
+			int x = zeroIndex.getX();
+			int y = zeroIndex.getY();
+
+			if (x > 0 && x <= size - 1)
+			{
+				Board board1 = new Board(board);
+				board1.swapCels(x, y, x - 1, y);
+				queue.enqueue(board1);
+			} else if (x < size - 1 && x >= 0)
+			{
+				Board board2 = new Board(board);
+				board2.swapCels(x, y, x + 1, y);
+				queue.enqueue(board2);
+			} else if (y > 0 && y <= size - 1)
+			{
+				Board board3 = new Board(board);
+				board3.swapCels(x, y, x, y - 1);
+				queue.enqueue(board3);
+
+			} else if (y < size - 1)
+			{
+				Board board4 = new Board(board);
+				board4.swapCels(x, y, x, y + 1);
+				queue.enqueue(board4);
+			}
+
 			return (Iterator<Board>) queue;
 		}
-
 	}
 
 	/**
@@ -129,7 +235,7 @@ public class Board
 	 * 
 	 * @return the 1d array of the board
 	 */
-	private int[] OneDimentionalConversion()
+	private int[] OneDimentionalConversion(int[][] board)
 	{
 		int[] returnable = new int[size * size];
 		int counter = 0;
@@ -137,7 +243,7 @@ public class Board
 		{
 			for (int j = 0; j < size; j++)
 			{
-				
+
 				returnable[counter] = board[i][j];
 				counter++;
 			}
@@ -145,24 +251,78 @@ public class Board
 		return returnable;
 	}
 
+	private Point FindIndexOfElement(int number)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (board[i][j] == number)
+				{
+					return new Point(i, j);
+				}
+			}
+		}
+		// Realistically, this should never happen
+		return null;
+	}
+
 	/**
-	 * This helper method takes two cels and swaps them according to cordinates from the parameters
-	 * @param r1 Row from First cel
-	 * @param c1 Column from First cel
-	 * @param r2 Row from destination cel
-	 * @param c2 Column from destination cel
+	 * This helper method takes two cels and swaps them according to cordinates
+	 * from the parameters
+	 * 
+	 * @param r1
+	 *            Row from First cel
+	 * @param c1
+	 *            Column from First cel
+	 * @param r2
+	 *            Row from destination cel
+	 * @param c2
+	 *            Column from destination cel
 	 */
 	private void swapCels(int r1, int c1, int r2, int c2)
 	{
-		if(r1 >= 0 && c1 >= 0 && r2 < board.length && c2 < board.length)
+		if (r1 >= 0 && c1 >= 0 && r2 < board.length && c2 < board.length)
 		{
 			int tempCelValue = board[r1][c1];
 			board[r1][c1] = board[r2][c2];
 			board[r2][c2] = tempCelValue;
-		}
-		else
+		} else
 		{
 			throw new IndexOutOfBoundsException("One of the row/column calls for some reason fell outside the board range");
+		}
+	}
+
+	private int calculate1DSpot(int row, int col)
+	{
+		int col1 = col;
+		int row1 = row;
+		col1++;
+		row1++;
+		int returnable = (((size) * row1) - (size)) + col1;
+		return returnable - 1;
+
+	}
+
+	private class Point
+	{
+		int x;
+		int y;
+
+		Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX()
+		{
+			return x;
+		}
+
+		public int getY()
+		{
+			return y;
 		}
 	}
 
--- tests/a04test/BoardTest.java
+++ tests/a04test/BoardTest.java
@@ -2,71 +2,183 @@ package a04test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import a04.Board;
 
-public class BoardTest
-{
-	int[][] testboard = {{1,2,3},{4,5,6},{7,8,0}};
-	Board b1 = new Board(testboard);
+public class BoardTest {
+
+	private final Board board1 = new Board(new int[][]{
+			{0}});
+	private final Board board2 = new Board(new int[][]{
+			{2,0},
+			{1,3}});
+	private final Board board3 = new Board(new int[][]{
+			{8, 1, 3},
+			{4, 0, 2},
+			{7, 6, 5}});
+	private final Board goalBoard = new Board(new int[][]{
+			{ 1,  2,  3,  4},
+			{ 5,  6,  7,  8},
+			{ 9, 10, 11, 12},
+			{13, 14, 15,  0}});
 	
-	@Before
-	public void setUp() throws Exception
-	{
+	@Test
+	public void testBoard_immutable() {
+		int[][] boardArray = {{8 ,1 ,3},{4 ,2 ,0},{7 ,6 ,5}};
+		Board immutableBoard = new Board(boardArray);
+		boardArray[1][2] = 2;
+		assertEquals("Class Board is not immutable", "3\n 8  1  3 \n 4  2  0 \n 7  6  5 \n", immutableBoard.toString());
+	}
+
+	@Test 
+	public void testSize() {
+		assertEquals(2, board2.size());
+		assertEquals(1, board1.size());
+		assertEquals(3, board3.size());
+		assertEquals(4, goalBoard.size());
+	}
+
+	@Test
+	public void testHamming() {
+		assertEquals(0, board1.hamming());
+		assertEquals(3, board2.hamming());
+		assertEquals(5, board3.hamming());
+		assertEquals(0, goalBoard.hamming());
+
+		Board offByOneBoard = new Board(new int[][]{{1 ,2 ,3},{4 ,5 ,6},{7 ,0 ,8}});
+		assertEquals(1, offByOneBoard.hamming());
+	}
+
+	@Test
+	public void testManhattan() {
+		assertEquals(0, board1.manhattan());
+		assertEquals(3, board2.manhattan());
+		assertEquals(10, board3.manhattan());
+		assertEquals(0, goalBoard.manhattan());
+
+		Board offByOneBoard = new Board(new int[][]{{1 ,2 ,3},{4 ,5 ,6},{7 ,0 ,8}});
+		assertEquals(1, offByOneBoard.manhattan());
+	}
+
+	@Test
+	public void testIsGoalTrue() {
+		assertEquals(true, board1.isGoal());
+		assertEquals(true, goalBoard.isGoal());
+	}
+
+	@Test
+	public void testIsGoalFalse() {
+		assertEquals(false, board2.isGoal());
+		assertEquals(false, board3.isGoal());
+	}
+	
+	@Test
+	public void testIsSolvableTrue() {
+		assertEquals(true, board3.isSolvable());
+		assertEquals(true, goalBoard.isSolvable());
+		assertEquals(true, board2.isSolvable());
+		assertEquals(true, board1.isSolvable());
+	}
+	
+	@Test
+	public void testIsSolvableFalse() {
+		Board notSolvable3 = new Board(new int[][]{{1 ,2 ,3},{4 ,5 ,6},{8 ,7 ,0}});
+		assertEquals(false, notSolvable3.isSolvable());
 		
+		Board notSolvable4 = new Board(new int[][]{{ 1,  2,  3,  4},
+				{ 5,  6,  7,  8}, { 9, 10, 11, 12},	{13, 15, 14,  0}});
+		assertEquals(false, notSolvable4.isSolvable());
 	}
 
 	@Test
-	public void testBoard()
-	{
-		Board b2 = new Board(testboard);
-		assertTrue(b2 != null);
+	public void testEqualsTrue() {
+		Board boardThree = new Board(new int[][]{{8, 1, 3},{4, 0, 2},{7, 6, 5}});
+		assertEquals(true, board3.equals(boardThree));
+		assertEquals(true, boardThree.equals(board3));
 	}
 	
 	@Test
-	public void testIsGoalBoard()
-	{
-		assertTrue(b1.isGoal());
-	}
-	
-	/**
-	 * This test case was to test that the 1d conversion is accurate.
-	 * The only reason why this is commented out is that the 1d conversion method is a private method,
-	 * and must be private for public api requirements
-	 */
-//	@Test
-//	public void test1DConversion()
-//	{
-//		int[] flat1 = b1.OneDimentionalConversion();
-//		String flat1conversion = toString(flat1);
-//		int[] testflat1 = {1,2,3,4,5,6,7,8,0};
-//		String testflat1conversion = toString(testflat1);
-//		assertTrue(flat1conversion.equals(testflat1conversion));
-//	}
-	
-	@Test
-	public void testSize()
-	{
-		assertTrue(b1.size() == 3);
+	public void testEqualsFalse() {
+		Board otherBoard3 = new Board(new int[][]{{8, 1, 3},{4, 6, 2},{7, 0, 5}});
+		assertEquals(false, board3.equals(otherBoard3));
+		assertEquals(false, otherBoard3.equals(board3));
 	}
 	
 	@Test
-	public void testHamming()
-	{
-		int i = b1.hamming();
-		assertTrue(i == 0);
+	public void testBoard1Neighbors() {		
+		Iterable<Board> neighbors = board1.neighbors();
+		assertEquals(0, count(neighbors));	
 	}
-	
-	public String toString(int[] array)
-	{
-		String returnable = "";
-		for(int item: array)
-		{
-			returnable +=item + " ";
+		
+	@Test
+	public void testBoard2Neighbors() {
+		Board[] board2Neighbors = {
+				new Board(new int[][]{{0,2},{1,3}}),
+				new Board(new int[][]{{2,3},{1,0}})
+				};
+		Iterable<Board> neighbors = board2.neighbors();
+		
+		assertEquals(board2Neighbors.length, count(neighbors));	// check number of neighbors 
+		
+		for (Board board : neighbors) {
+			// use contains because neighbors can be listed in arbitrary order
+			if (!Arrays.asList(board2Neighbors).contains(board))
+				fail(board.toString() + " is not a neighbor of " + board2.toString());
 		}
-		return returnable;
+	}
+	
+	@Test
+	public void testBoard3Neighbors() {
+		Board[] board3Neighbors = {
+				new Board(new int[][]{{8, 1, 3},{4, 2, 0},{7, 6, 5}}),
+				new Board(new int[][]{{8, 1, 3},{0, 4, 2},{7, 6, 5}}),
+				new Board(new int[][]{{8, 1, 3},{4, 6, 2},{7, 0, 5}}),
+				new Board(new int[][]{{8, 0, 3},{4, 1, 2},{7, 6, 5}})
+				};		
+		Iterable<Board> neighbors = board3.neighbors();
+		
+		assertEquals(board3Neighbors.length, count(neighbors));	// check number of neighbors 
+
+		for (Board board : neighbors) { 
+			// use contains because neighbors can be listed in arbitrary order
+			if (!Arrays.asList(board3Neighbors).contains(board))
+				fail(board.toString() + " is not a neighbor of " + board3.toString());
+		}
 	}
 
+	@Test
+	public void testGoalBoardNeighbors() {
+		Board[] boardGoal = {
+				new Board(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,0},{13,14,15,12}}),
+				new Board(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,0,15}})
+				};		
+		Iterable<Board> neighbors = goalBoard.neighbors();
+		
+		assertEquals(boardGoal.length, count(neighbors));
+		
+		for (Board board : neighbors) {
+			if (!Arrays.asList(boardGoal).contains(board))
+				fail(board.toString() + " is not a neighbor of " + goalBoard.toString());
+		}
+	}
+
+	@Test
+	public void testToString() {
+		assertEquals("1\n 0 \n", board1.toString());
+		assertEquals("2\n 2  0 \n 1  3 \n", board2.toString());
+		assertEquals("3\n 8  1  3 \n 4  0  2 \n 7  6  5 \n", board3.toString());
+		assertEquals("4\n 1  2  3  4 \n 5  6  7  8 \n 9 10 11 12 \n13 14 15  0 \n", goalBoard.toString());
+	}
+	
+	// - - - helper methods	
+	private int count(Iterable<Board> boards) {
+		int count = 0;
+		for (Board b : boards)
+			count++;
+		
+		return count;
+	}
 }
